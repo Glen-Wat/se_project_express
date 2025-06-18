76886aa23699df61562c43af173791632a9c4a21
Glen Watson <watsonglen4@gmail.com>
2025-06-18 00:34:43 -0700

    clothingItem shcema

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,5 +1,41 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const clothingItemSchema = new mongoose.Schema({});
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageURL: {
+    type: String,
+    required: true,
+    validator(value) {
+      return validator.isURL(value);
+    },
+    message: "You must enter a valid URL.",
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 modules.exports = mongoose.model("item", clothingItemSchema);
